c4abd4c1d1fb8ec42d4821dac20c4f3a71f6b76d
MS1 <ms1@MS1s-iMac.local>
2017-09-12 17:46:46 +0400

    Completed

--- CoreDataSample/ViewController.swift
+++ CoreDataSample/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     fileprivate let cellId = "cellId"
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         title = "The List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        guard let app = UIApplication.shared.delegate as? AppDelegate else {return}
+        let moc = app.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            
+            people = try moc.fetch(fetchRequest)
+            
+        } catch let error as NSError{
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +50,7 @@ class ViewController: UIViewController {
         
        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] (action) in
         guard let textField = alert.textFields?.first, let nameToSave = textField.text else {return}
-        self.names.append(nameToSave)
+        self.save(name : nameToSave)
         self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel",
@@ -46,13 +61,32 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    fileprivate func save(name: String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let moc = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: moc)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: moc)
+        
+        person.setValue(name, forKey: "name")
+       
+        do {
+            try moc.save()
+            people.append(person)
+            
+        } catch let error as NSError{
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
     
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath)
@@ -60,6 +94,7 @@ extension ViewController: UITableViewDataSource {
             let cell =
                 tableView.dequeueReusableCell(withIdentifier: cellId,
                                               for: indexPath)
-            cell.textLabel?.text = names[indexPath.row]
+            let person = people[indexPath.row]
+            cell.textLabel?.text = person.value(forKeyPath: "name") as? String
             return cell }
 }
